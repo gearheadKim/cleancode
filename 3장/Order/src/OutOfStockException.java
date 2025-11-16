e0bf592df7241727bc63bd82768dbef599540e97
gearheadKim <adsr2001@gmail.com>
2025-11-16 20:34:06 +0900

    Delete 3장/Order/src directory

--- 3장/Order/src/OutOfStockException.java
+++
@@ -1,7 +0,0 @@
-package Order.src;
-
-public class OutOfStockException extends RuntimeException {
-    OutOfStockException(String message) {
-        super(message);
-    }
-}
--- 3장/Order/src/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package Order.src;
-
-public class ValidationException extends RuntimeException {
-    ValidationException(String message) {
-        super(message);
-    }
-}
